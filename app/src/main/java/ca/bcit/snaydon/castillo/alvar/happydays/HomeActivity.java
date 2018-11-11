f29b2363efe2ddd9ee247e44a3577038feb222d6
Kiaan Castillo <kiaancastillo@outlook.com>
2018-11-11 13:30:19 -0800

    made fragments for each menu item + bottom nav and fab goes to correct fragments when clicked

--- app/src/main/java/ca/bcit/snaydon/castillo/alvar/happydays/HomeActivity.java
+++ app/src/main/java/ca/bcit/snaydon/castillo/alvar/happydays/HomeActivity.java
@@ -1,45 +1,56 @@
 package ca.bcit.snaydon.castillo.alvar.happydays;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
-
-    private BottomNavigationView bottomNavigationView;
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.navigation_home:
-                                // TODO
-                                return true;
-                            case R.id.navigation_logs:
-                                // TODO
-                                return true;
-                            case R.id.navigation_profile:
-                                // TODO
-                                return true;
-                            case R.id.navigation_menu:
-                                // TODO
-                                return true;
-                        }
-                        return false;
-                    }
-                });
+        loadFragment(new HomeFragment());
     }
 
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
+    }
 
+    public void loadActivities(View v) {
+        Fragment fragment = new ActivitiesFragment();
+        loadFragment(fragment);
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.navigation_logs:
+                fragment = new LogsFragment();
+                break;
+            case R.id.navigation_profile:
+                fragment = new ProfileFragment();
+                break;
+            case R.id.navigation_menu:
+                fragment = new MenuFragment();
+                break;
+        }
+
+        return loadFragment(fragment);
+    }
 }
