0950ac8a7028816ffc18ae15302d5febed7903c7
Kiaan Castillo <kiaancastillo@outlook.com>
2018-11-10 23:05:59 -0800

    initial commit

---
+++ app/src/main/java/ca/bcit/snaydon/castillo/alvar/happydays/HomeActivity.java
@@ -0,0 +1,21 @@
+package ca.bcit.snaydon.castillo.alvar.happydays;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bottom_navigation, menu);
+        return true;
+    }
+}
