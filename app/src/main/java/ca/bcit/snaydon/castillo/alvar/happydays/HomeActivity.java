9ca42ac63f5dc7d05a5f0072e82429e6b258daa0
Kiaan Castillo <kiaancastillo@outlook.com>
2018-11-11 12:14:08 -0800

    added listeners for bottom nav items

--- app/src/main/java/ca/bcit/snaydon/castillo/alvar/happydays/HomeActivity.java
+++ app/src/main/java/ca/bcit/snaydon/castillo/alvar/happydays/HomeActivity.java
@@ -1,21 +1,45 @@
 package ca.bcit.snaydon.castillo.alvar.happydays;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_home:
+                                // TODO
+                                return true;
+                            case R.id.navigation_logs:
+                                // TODO
+                                return true;
+                            case R.id.navigation_profile:
+                                // TODO
+                                return true;
+                            case R.id.navigation_menu:
+                                // TODO
+                                return true;
+                        }
+                        return false;
+                    }
+                });
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.bottom_navigation, menu);
-        return true;
-    }
+
+
 }
